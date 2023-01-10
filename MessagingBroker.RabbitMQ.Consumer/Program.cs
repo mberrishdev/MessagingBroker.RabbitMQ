894deeb037518c16fa1f76eb136c94440ced4c5a
mberrish <mikheil.berishvili12@gmail.com>
2023-01-10 16:54:34 +0400

    Add project files.

---
+++ MessagingBroker.RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+Console.WriteLine("Hello, World from Consumer!");
+
+var factory = new ConnectionFactory
+{
+    Uri = new Uri("amqp://guest:guest@localhost:5672"),
+};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare("sample-rabbitMq", true, false, false, null);
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (sender, args) =>
+{
+    var body = args.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine(message);
+};
+
+channel.BasicConsume("sample-rabbitMq", true, consumer);
+Console.ReadLine();
+
---
+++ MessagingBroker.RabbitMQ.Producer/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
+
+Console.WriteLine("Hello, World from Producer!");
+
+var factory = new ConnectionFactory
+{
+    Uri = new Uri("amqp://guest:guest@localhost:5672"),
+};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare("sample-rabbitMq", true, false, false, null);
+
+while (true)
+{
+    var input = Console.ReadLine();
+    if (input.Equals("exit", StringComparison.OrdinalIgnoreCase))
+    {
+        break;
+    }
+    var message = new { Name = "Producer", Message = input };
+    var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+
+    channel.BasicPublish("", "sample-rabbitMq", null, body);
+}
+
+
